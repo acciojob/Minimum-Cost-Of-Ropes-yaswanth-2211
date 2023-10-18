6c5779aaecb2862f0bfc276caf46a957c6e74fa9
yaswanth-2211 <145182914+yaswanth-2211@users.noreply.github.com>
2023-10-18 14:39:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function calculateMinCost() {
-  //your code here
-  
-  
-  
-}  
+	let a=document.getElementById.innerHTML("text");
+	let array=a.split(",").map(Number);
+	let sum=0;
+	    while(array.length>1){
+	    array.sort(function(a, b){return a - b});
+		let c=array[0]+array[1];
+		sum=sum+c;
+		array.splice(0,2,c);
+		}
+		document.getElementById("result").innerHTML = sum;
+	}
